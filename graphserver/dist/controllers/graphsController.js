c4c9e9184389b29567820fe7ead6e1dae5368d8a
Mason England <masonengland01@gmail.com>
2023-06-26 13:21:03 -0600

    small cleanup

--- graphserver/dist/controllers/graphsController.js
+++ graphserver/dist/controllers/graphsController.js
@@ -1,14 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Model = require('../models/Schemas.js');
+// hold all models in a list to iterate on later
 const modelList = [
     Model.vendiaModel,
     Model.gChartModel
 ];
+// function to pull all graphs for a specifica account
 exports.getGraphs = async (req, res) => {
     const id = req.params.id;
     let graphList = [];
     try {
+        // search each db document for graphs linked
+        // to the account ID
         for (let item of modelList) {
             let docs = await item.find({ accountID: id });
             graphList.push(docs);
--- graphserver/dist/routes/graphs.js
+++ graphserver/dist/routes/graphs.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 const controller = require("../controllers/graphsController.js");
+// all routes for /graphs
 router.post("/:type", controller.registerGraph);
 router.get("/:id", controller.getGraphs);
 module.exports = router;
