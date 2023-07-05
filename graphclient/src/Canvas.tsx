2b79707516f7341a26a9dca49465a4c585b823ec
Mason England <masonengland01@gmail.com>
2023-07-05 17:37:35 -0600

    very small change

--- graphclient/src/Canvas.tsx
+++ graphclient/src/Canvas.tsx
@@ -1,10 +1,11 @@
+import p5 from 'p5';
 import './canvas.css'
 import vendiagram from './sketches/vendiagram';
 const { ReactP5Wrapper } = require('react-p5-wrapper');
 
 export default function Canvas() {
   return (
-     <ReactP5Wrapper sketch={vendiagram}/>
+     <ReactP5Wrapper sketch={(p: p5) => vendiagram(p)}/>
   );
 };
   
