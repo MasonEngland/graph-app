702fb910353ea9ec31d718934b8ffde58c90a55f
Mason England <masonengland01@gmail.com>
2023-07-06 16:18:32 -0600

    added vendiagram sketch

--- graphclient/src/sketches/vendiagram.ts
+++ graphclient/src/sketches/vendiagram.ts
@@ -3,11 +3,26 @@ import react from 'react';
 
 export default function vendiagram(p: p5) {
 
+    const labelContents: string[] = [];
+    const labels: p5.Element[] = [];
+
+
     p.setup = () => {
-        const canvas = p.createCanvas(600, 300);
+        const canvas = p.createCanvas(800, 500);
         canvas.position(undefined, undefined, 'relative');
+        drawDiagram();
     }
-    p.draw = () => {
-        p.circle(100, 100, 50);
+    
+    const drawDiagram = () => {
+        p.blendMode(p.MULTIPLY);
+        for (let i = 0; i < 2; i++) {
+            labels.push(p.createElement('h3', "Label Here"));
+            p.circle(300 + (i * 180), 250, 380);
+        }
+        for (let i = 0; i < labels.length; i++) {
+            labels[i].class(`vendiagram label${i}`);
+            labels[i].attribute("contenteditable", "true");
+            labels[i].size(150, 25);
+        }
     }
 }
--- graphserver/dist/models/Schemas.js
+++ graphserver/dist/models/Schemas.js
@@ -49,10 +49,12 @@ const gChartSchema = new Schema({
     accountID: String,
     startTime: Number,
     EndTime: Number,
+    GanttTitle: String,
     Tasks: [{
             TaskStartTime: Number,
             TaskEndTime: Number,
-            TaskName: String
+            TaskName: String,
+            TaskPerson: String
         }]
 });
 const accountModel = mongoose.model("Accounts", accountSchema);
