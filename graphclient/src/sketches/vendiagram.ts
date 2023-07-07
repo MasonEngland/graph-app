bd7776ab77926d78ee3a4ad0a528b468538d3e9e
Mason England <masonengland01@gmail.com>
2023-07-07 15:44:23 -0600

    finished venn diagrams

--- graphclient/src/sketches/vendiagram.ts
+++ graphclient/src/sketches/vendiagram.ts
@@ -1,10 +1,13 @@
+import { render } from '@testing-library/react';
 import p5 from 'p5';
-import react from 'react';
+import react, { useState } from 'react';
 
 export default function vendiagram(p: p5) {
 
     const labels: p5.Element[] = [];
-    const elements: p5.Element[] = [];
+    let leftnotes: p5.Element[] = [];
+    let rightnotes: p5.Element[] = [];
+    let middlenotes: p5.Element[] = [];
 
     p.setup = () => {
         const canvas = p.createCanvas(800, 500);
@@ -28,12 +31,40 @@ export default function vendiagram(p: p5) {
 
     const createAddButtons = () => {
         const buttons: p5.Element[] = [
-            p.createButton("+", "Hello").position(580, 280, 'absolute'),
-            p.createButton("+", "Hello").position(838, 230, 'absolute'),
-            p.createButton("+", "Hello").position(1100, 280, 'absolute'),
+            p.createButton("+", "Hello").position(580, 280, 'absolute').mousePressed(() => {renderNotes("left")}),
+            p.createButton("+", "Hello").position(838, 230, 'absolute').mousePressed(() => {renderNotes("middle")}),
+            p.createButton("+", "Hello").position(1100, 280, 'absolute').mousePressed(() => {renderNotes("right")}),
         ];
         for (let item of buttons) {
             item.addClass("add-button");
+        }
+        renderNotes("left");
+        renderNotes("right");
+        renderNotes("middle");
+    }
+
+    const renderNotes = (location: string) => {
+        switch (location) {
+            case "left":
+                leftnotes.push(p.createElement("h4", "New Item<br />"));
+                for (let i = 0; i < leftnotes.length; i++) {
+                    leftnotes[i].position(630, 300 + (i * 30), 'absolute');
+                    leftnotes[i].attribute('contenteditable', 'true');
+                }
+                break;
+            case "right":
+                rightnotes.push(p.createElement("h4", "New Item"));
+                for (let i = 0; i < rightnotes.length; i++) {
+                    rightnotes[i].position(1000, 300 + (i * 30), 'absolute');
+                    rightnotes[i].attribute('contenteditable', 'true');
+                }
+                break;
+            case "middle":
+                middlenotes.push(p.createElement("h4", "New Item"));
+                for (let i = 0; i < middlenotes.length; i++) {
+                    middlenotes[i].position(815, 300 + (i * 30), 'absolute');
+                    middlenotes[i].attribute("contenteditable", "true");
+                }
         }
     }
 }
