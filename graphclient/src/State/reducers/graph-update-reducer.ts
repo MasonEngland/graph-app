8a4dac1d3ce412a99e1b27cdafb317f39f0aea6c
Mason England <masonengland01@gmail.com>
2023-07-21 21:14:25 -0600

    small bug fixes

--- graphclient/src/State/reducers/graph-update-reducer.ts
+++ graphclient/src/State/reducers/graph-update-reducer.ts
@@ -4,11 +4,13 @@ import { ActionType } from "../action-types/user-action-types";
 
 
 type graphstate = {
-    currentGraph: any | null
+    currentGraph: any | null,
+    graphType: string| null
 }
 
 const initialState: graphstate = {
-    currentGraph: null
+    currentGraph: null,
+    graphType: null
 }
 
 type AppAction = Action | SagaProfileAction 
@@ -17,7 +19,7 @@ const updateGraphReducer = (state = initialState, action: AppAction ) => {
     switch (action.type) {
         case ActionType.UPDATE_BAR_GRAPH:
             //console.log(action.payload);
-            return {...state, currentGraph: action.payload}
+            return {...state, currentGraph: action.payload, graphType: "bargraph"};
         default:
             return state
     }
